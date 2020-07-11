63f46255cd8678020763d1f90b7992cb6fd475ba
Timotej <tim.nikic@gmail.com>
2020-07-11 16:14:46 +0200

    Second Commit

---
+++ routes/authors.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const author = require('../models/author')
+const router = express.Router()
+const Author = require('../models/author')
+
+//All authors Route
+router.get('/', async (req, res) => {
+    try {
+        const authors = await Author.find({})
+        res.render('authors/index, { authors: authors')
+    } catch {
+        res.redirect('/')
+    }
+})
+
+// New Author Route
+router.get('/new', (req, res) => {
+    res.render('authors/new', { author: new author() })
+})
+
+// Create Author Route
+router.post('/', async (req, res) => {
+    const author = new Author({
+        name: req.body.name
+    })
+    try {
+        const newAuthor = await author.save()
+        //res.redirect(`authors/${newAuthor}`)
+        res.redirect('authors')
+    } catch {
+    res.render('authors/new', {
+        author: author,
+        errorMessage: 'Error creating Author'
+    })}
+})
+    
+// WITHOUT ASYNC AWAIT
+/*author.save((err, newAuthor) => {
+        if( err ) {
+            res.render('authors/new', {
+                author: author,
+                errorMessage: 'Error creating Author'
+            })
+        }
+        else {
+            //res.redirect(`authors/${newAuthor}`)
+            res.redirect('authors')
+        }
+    })*/
+
+module.exports = router
--- server.js
+++ server.js
@@ -5,14 +5,17 @@ if(process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
     //pa poglejmo ce dela
 app.set('view engine', 'ejs')
 app.set('views', './views')
 app.set('layout', 'layout') //we dont need to duplicate header and footers of html files
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded( {limit: '10mb', extended: false} ))
 
 const mongoose = require('mongoose')
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -21,5 +24,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
